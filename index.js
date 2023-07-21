e32eed574d64b891ae271f060091a86ce9be63f5
Natalies-Angels <natalie.shikuku@student.moringaschool.com>
2023-07-21 10:09:09 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+function divide(dividend,divisor)
+{
+  return dividend/divisor
+}
+console.log(divide(2000,100));
+
+const square = x => x*x;
+square(2);
+
+const add = (num1,num2) => num1 + num2;
+add(2,3);
